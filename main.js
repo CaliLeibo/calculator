55c59375951b2ca92c812955d567432396b565f4
Cali Leibovitz <calileibo@yahoo.com>
2024-07-26 16:53:48 -0400

    Add limit to numbers you can put in calculator

--- main.js
+++ main.js
@@ -2,42 +2,53 @@ const display = document.querySelector('.display');
 const clear = document.querySelector('.clear');
 const numbers = document.querySelectorAll('.num');
 const zero = document.querySelector('.zero');
-let input1 = [];
-
-console.log(input1);
+const operator = document.querySelectorAll('.operator');
+let input = [];
+let num1 = 0;
 
 numbers.forEach(button => button.addEventListener('click', function() {
-    if(button.textContent === '1')
-        input1.push('1');
-    if(button.textContent === '2')
-        input1.push('2');
-    if(button.textContent === '3')
-        input1.push('3');
-    if(button.textContent === '4')
-        input1.push('4');
-    if(button.textContent === '5')
-        input1.push('5');
-    if(button.textContent === '6')
-        input1.push('6');
-    if(button.textContent === '7')
-        input1.push('7');
-    if(button.textContent === '8')
-        input1.push('8');
-    if(button.textContent === '9')
-        input1.push('9');
+    if(button.textContent === '1' && input.length < 10)
+        input.push('1');
+    if(button.textContent === '2' && input.length < 10)
+        input.push('2');
+    if(button.textContent === '3' && input.length < 10)
+        input.push('3');
+    if(button.textContent === '4' && input.length < 10)
+        input.push('4');
+    if(button.textContent === '5' && input.length < 10)
+        input.push('5');
+    if(button.textContent === '6' && input.length < 10)
+        input.push('6');
+    if(button.textContent === '7' && input.length < 10)
+        input.push('7');
+    if(button.textContent === '8' && input.length < 10)
+        input.push('8');
+    if(button.textContent === '9' && input.length < 10)
+        input.push('9');
     
-    console.log(input1);
-    display.textContent= input1.join('');
+    num1 = input.join('')
+    display.textContent= num1;    
+    numerator1 = parseInt(num1);
+    console.log(`${numerator1} ` + typeof numerator1);
 }));
 
 zero.addEventListener('click', () => {
-    input1.push('0');
-    console.log(input1);
-    display.textContent= input1.join('');
+    input.push('0');
+    console.log(input);
+    display.textContent= input.join('');
 })
+
+operator.forEach((op) => op.addEventListener('click', function() {
+    if (op.textContent === '+') {
+    }
+}));
 
 clear.addEventListener('click', () => {
     display.textContent = '0';
-    input1 = [];
-    console.log(input1);
+    input = [];
+    console.log(input);
 })
+
+function computeSum(arr) {
+    let num = parseInt(arr);
+}
